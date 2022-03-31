8b73654cfaeb91f8e4a68f66cb8ccbb96619ddf0
AndrewYatzkan <andrew@yatzkan.com>
2022-03-31 00:16:26 -0500

    cleaned up

--- web/server/server.js
+++ web/server/server.js
@@ -1,5 +1,5 @@
 const twilio = require('./twilio.js');
-const {generateUID, UID_REGEX_PATTERN, orderExists} = require('./helpers.js');
+const { UID_REGEX_PATTERN, orderExists, createOrder, submitOrder } = require('./helpers.js');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -8,14 +8,34 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 const port = 3000;
 
+// orders = {
+//   'Chjo': {
+//     user: '+16305452222',
+//     status: 'created'
+//   }
+// }
+// ordersByUser = {
+//   '+16305452222': ['Chjo']
+// }
 orders = {};
 ordersByUser = {};
-ingredients = ['Nesquik Powder', 'Milk', 'Orange Juice'];
+all_ingredients = ['Nesquik Powder', 'Milk', 'Orange Juice'];
+bottles = {
+  1: 'Nesquik Powder',
+  2: 'Milk',
+  3: 'Orange Juice',
+  4: '', 5: '', 6: '', 7: '', 8: '', 9: ''
+};
+available_ingredients = Object.values(bottles).filter(x => x);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 
 app.use(express.static('../public'));
+
+app.get('/admin', (req, res) => {
+  res.sendFile(`admin.html`, { root: `${__dirname}/../public` });
+});
 
 app.get(`/:orderID(${UID_REGEX_PATTERN})`, (req, res) => {
   let orderID = req.params.orderID;
@@ -37,17 +57,13 @@ app.post(`/order/:orderID(${UID_REGEX_PATTERN})`, (req, res) => {
     // 'ratio' key must be a number with at most 2 decimal places (to avoid floating point errors with sum)
     if (!ratio || isNaN(ratio) || ratio != ratio.toFixed(2)) return res.sendStatus(400);
     if (!name) return res.status(400).send('Drink name not supplied');
-    if (!ingredients.includes(name)) return res.status(417).send('One or more of the ingredients are out of stock');
+    if (!available_ingredients.includes(name)) return res.status(417).send('One or more of the ingredients are out of stock');
   }
   if (!drinkIngredients.length) return res.status(400).send('No ingredients supplied');
   if (ratioSum != 1) return res.status(400).send('Ingredient ratios do not sum to 100%');
-  orders[orderID].drink = {
-    name: drinkName,
-    ingredients: drinkIngredients ? drinkIngredients : ingredients[drinkName]
-  };
-  orders[orderID].status = 'created';
+  submitOrder(orderID, drinkName, drinkIngredients);
   // console.log('New order:', orderID, drinkName, drinkIngredients);
-  res.send(orders);
+  res.sendStatus(200);
 });
 
 app.post('/sms', (req, res) => {
@@ -56,18 +72,16 @@ app.post('/sms', (req, res) => {
   console.log(`Message from: ${from}\nContent: ${body}`);
 
   let response = '';
-  if (body === 'order') {
-    let orderID = generateUID(Object.keys(orders));
-    orders[orderID] = {user: from};
-    if (!ordersByUser[from]) ordersByUser[from] = [];
-    ordersByUser[from].push(orderID);
+  if (body.toLowerCase() === 'order') {
+    let orderID = createOrder(from);
     console.log(`Order ID ${orderID} generated for ${from}`);
-    response = `http://f8c3-130-126-255-204.ngrok.io/${orderID}`;
-  } else if (body === 'status') {
+    response = `http://038e-96-63-255-28.ngrok.io/${orderID}`;
+  } else if (body.toLowerCase() === 'status') {
     let userOrders = ordersByUser[from];
     for (orderID of userOrders) {
       let order = orders[orderID];
-      response += `Order #${orderID} status: ${order.status}\n`;
+      // response += `Order #${orderID} status: ${order.status}\n`;
+      response += `Your ${order.drink.name} is ${order.status == 'created' ? 'still in the queue!' : '???'}\n`;
     }
     response = response.trim();
   }
@@ -80,7 +94,16 @@ app.post('/sms', (req, res) => {
 
 io.on('connection', socket => {
   console.log('A user connected.');
-  socket.emit('available ingredients', ingredients);
+  socket.emit('available ingredients', available_ingredients);
+
+  socket.on('fetch all ingredients', callback => {
+    callback(all_ingredients);
+    socket.emit('bottle statuses', bottles);
+  });
+
+  socket.on('update bottles', bottles_ => {
+    bottles = bottles_;
+  });
 });
 
 server.listen(port, () => {
