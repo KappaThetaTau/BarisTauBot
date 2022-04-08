00eba1ad7cb749ace620b7c5f6901a942b8fb1b1
AndrewYatzkan <andrew@yatzkan.com>
2022-04-08 05:07:30 -0500

    admins list updated

--- web/server/admins.js
+++ web/server/admins.js
@@ -1,3 +1,6 @@
 module.exports = [
-	'+16305452222' // Andrew Yatzkan
+	'+16305452222', // Andrew Yatzkan
+	'+16303980226', // Emily Hettinger
+	'+19786213677', // Sarah O'Sullivan
+	'+14089216032' // Kunal Sheth
 ];
